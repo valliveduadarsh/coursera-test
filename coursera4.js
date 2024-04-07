0bb06e1ac743dcd63d81dbf4211ce7f19025ba45
valliveduadarsh <137595560+valliveduadarsh@users.noreply.github.com>
2024-04-07 14:34:13 +0530

    Add files via upload

---
+++ coursera4.js
@@ -0,0 +1,21 @@
+var name=new Array();
+names[0]"adarsh";
+names[1]"ganesh";
+names[2]"rabbani";
+names[3]"kishor";
+names[4]"sai";
+names[5]"naresh";
+names[6]"madhan";
+names[7]"jaswanth";
+names[8]"murali";
+console.log(names[0].ChartAt(0));
+for (var i = 0; i < names.length; i++){
+   if(n[i].chartAt(0)==='j'|| n[i].chartAt(0)==='j'){
+    console.log("goodbye "+ names[i])
+  }
+  eles{
+    console.log("hello "+ names[i])
+}
+
+
+
